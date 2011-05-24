3a0b7f7f6aac4c5aad8064932633518b959a7840
Stig Brautaset <stig@brautaset.org>
2011-05-24 23:15:54 +0800

    New skeleton sample project

---
+++ TweetStream/TweetStream/TweetStreamViewController.h
@@ -0,0 +1,15 @@
+//
+//  TweetStreamViewController.h
+//  TweetStream
+//
+//  Created by Stig Brautaset on 24/05/2011.
+//  Copyright 2011 Stig Brautaset. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TweetStreamViewController : UIViewController {
+    
+}
+
+@end
