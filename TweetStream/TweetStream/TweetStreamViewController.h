859535e83581578b06aa96178fb5c7d1e707b777
Stig Brautaset <stig@brautaset.org>
2011-05-25 08:46:11 +0800

    Authenticate with Twitter and get a never-ending stream of tweets

--- TweetStream/TweetStream/TweetStreamViewController.h
+++ TweetStream/TweetStream/TweetStreamViewController.h
@@ -8,8 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
+@class SBJsonStreamParser;
+@class SBJsonStreamParserAdapter;
+
 @interface TweetStreamViewController : UIViewController {
+    IBOutlet UITextField *username;
+    IBOutlet UITextField *password;
+    IBOutlet UITextView *tweet;
     
+    SBJsonStreamParser *parser;
+    SBJsonStreamParserAdapter *adapter;
 }
+
+- (IBAction)go;
 
 @end
